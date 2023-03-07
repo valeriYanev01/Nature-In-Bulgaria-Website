3d0b0bf1070130817baff640fa3fe5b6d6c26273
valeriYanev01 <valeri.t.yanev@gmail.com>
2023-03-07 19:17:02 +0200

    adding functionality to the buttons on the front page

--- Vacation/pages/front/app.js
+++ Vacation/pages/front/app.js
@@ -1,12 +1,22 @@
-const buttonEl = document.body.querySelector("button");
-const centerEl = document.body.querySelector(".el--center");
-const infoHeightEl = document.body.querySelector(".el--info--height");
+const firstButtonEl = document.body.querySelector(".el--button--first");
+const secondButtonEl = document.body.querySelector(".el--button--second");
+const thirdButtonEl = document.body.querySelector(".el--button--third");
 
-const transform = () => {
-  console.log("hello");
-  infoHeightEl.classList.remove("el--center");
+const transformContent = (e) => {
+  const wrapperEl = e.currentTarget.parentElement.previousSibling.previousSibling;
+  const additionalContentEl =
+    e.currentTarget.parentElement.previousSibling.previousSibling.previousSibling.previousSibling;
+  if (wrapperEl.style.display === "none") {
+    wrapperEl.style.display = "block";
+    additionalContentEl.style.display = "none";
+  } else {
+    wrapperEl.style.display = "none";
+    additionalContentEl.style.display = "block";
+  }
+  console.log(wrapperEl);
+  console.log(additionalContentEl);
 };
 
-buttonEl.addEventListener("click", transform);
-
-console.log("heelo");
+firstButtonEl.addEventListener("click", transformContent);
+secondButtonEl.addEventListener("click", transformContent);
+thirdButtonEl.addEventListener("click", transformContent);
