31597ba022a87e9280f40b759aef8559ca93415e
valeriYanev01 <valeri.t.yanev@gmail.com>
2023-08-29 19:35:42 +0300

    adding functionality to retrieve data on click about every img element and restore the old data on back button click

--- Nature In Bulgaria/pages/tourism Types/app.js
+++ Nature In Bulgaria/pages/tourism Types/app.js
@@ -1,16 +1,50 @@
-import { fetchItems } from "./fetchingData.js";
+import { fetchDescription, fetchData } from "./fetchingData.js";
 
 const imgEl = document.querySelectorAll(".grid--img--list");
 const gridEl = document.querySelector(".grid");
 const img = document.querySelector(".img");
 const imgContainerEl = document.querySelector(".img--container");
 const headingBtnEl = document.querySelector(".heading-button");
-
+const headingTitleEl = document.querySelector(".heading-title");
 const destinationInfoEl = document.querySelector(".destinations--info");
 
-destinationInfoEl.innerHTML = await fetchItems().then((tour) => {
+destinationInfoEl.innerHTML = await fetchDescription().then((tour) => {
   return tour.description;
 });
+
+async function buildTitleAndDescription(description, title, el) {
+  description = await fetchData(el).then((desc) => {
+    return desc.tourDescription;
+  });
+  title = await fetchData(el).then((desc) => {
+    return desc.tourName;
+  });
+  headingTitleEl.textContent = title;
+  destinationInfoEl.innerHTML = description;
+}
+
+async function showDescription(el) {
+  destinationInfoEl.innerHTML = "";
+  headingTitleEl.textContent = "";
+  let description;
+  let title;
+
+  if (imgEl[el].classList.contains("fifth")) {
+    buildTitleAndDescription(description, title, 0);
+  }
+
+  if (imgEl[el].classList.contains("sixth")) {
+    buildTitleAndDescription(description, title, 1);
+  }
+
+  if (imgEl[el].classList.contains("seventh")) {
+    buildTitleAndDescription(description, title, 3);
+  }
+
+  if (imgEl[el].classList.contains("eighth")) {
+    buildTitleAndDescription(description, title, 2);
+  }
+}
 
 function hideGrid() {
   gridEl.classList.add("hide");
@@ -27,7 +61,8 @@ async function backBtn() {
   gridEl.classList.remove("hide");
   gridEl.classList.remove("hidden");
   headingBtnEl.classList.add("hidden");
-  destinationInfoEl.innerHTML = await fetchItems().then((tour) => {
+  headingTitleEl.textContent = "Tourism in Bulgaria";
+  destinationInfoEl.innerHTML = await fetchDescription().then((tour) => {
     return tour.description;
   });
 }
@@ -47,27 +82,26 @@ function showImgContainer(el) {
     headingBtnEl.classList.remove("hidden");
 
     if (imgEl[el].classList.contains("sixth")) {
-      imgBuild("../../img/tourism--page/black-sea-header-img.jpg", "Black Sea");
+      imgBuild("../../img/tourism--page/black-sea-header-img.jpg");
     }
     if (imgEl[el].classList.contains("fifth")) {
       img.style.objectViewBox = "inset(50% 10% 10% 2%)";
-      imgBuild("../../img/tourism--page/ski-header-img.jpg", "Ski");
+      imgBuild("../../img/tourism--page/ski-header-img.jpg");
     }
     if (imgEl[el].classList.contains("seventh")) {
-      imgBuild("../../img/tourism--page/eco-header-img.jpg", "Eco");
+      imgBuild("../../img/tourism--page/eco-header-img.jpg");
     }
     if (imgEl[el].classList.contains("eighth")) {
-      imgBuild("../../img/tourism--page/cultural-header-img.jpg", "Culture");
+      imgBuild("../../img/tourism--page/cultural-header-img.jpg");
     }
   }, 550);
   img.style.objectViewBox = "none";
 }
 
-function imgBuild(src, content) {
+function imgBuild(src) {
   img.src = src;
   img.style.borderRadius = "20px";
   imgContainerEl.appendChild(img);
-  destinationInfoEl.textContent = content;
   imgContainerEl.classList.add("show");
 }
 
@@ -76,6 +110,7 @@ for (let i = 0; i < imgEl.length; i++) {
   imgEl[i].addEventListener("click", function () {
     hideGrid();
     showImgContainer(i);
+    showDescription(i);
   });
 }
 
--- Nature In Bulgaria/pages/tourism Types/fetchingData.js
+++ Nature In Bulgaria/pages/tourism Types/fetchingData.js
@@ -1,5 +1,11 @@
-export async function fetchItems() {
+export async function fetchDescription() {
   const response = await fetch("https://api.jsonbin.io/v3/b/64ece29cb89b1e2299d72443");
   const tourisms = await response.json();
   return tourisms.record.tourism[0].General[0];
+}
+
+export async function fetchData(el) {
+  const response = await fetch("https://api.jsonbin.io/v3/b/64ece29cb89b1e2299d72443");
+  const tourisms = await response.json();
+  return tourisms.record.tourism[0].Types[el];
 }
